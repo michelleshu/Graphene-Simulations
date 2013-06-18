0d8d20e53eb3ec9a74a597b94a16fcb4673337e0
Michelle Shu <simplevity@gmail.com>
2013-06-18 11:40:08 -0400

    initial plots of double layer potential

--- pb_3p_double_layer.m
+++ pb_3p_double_layer.m
@@ -1,4 +1,4 @@
-function [ X, P ] = pb_3p_double_layer
+function [ X, P ] = pb_3p_double_layer (P_0, C_0, E_R)
 % PB_3P_DOUBLE_LAYER Simulation of electrical double layer using 
 % Poisson-Boltzmann distribution and 3-point formula. Assumes point charge, 
 % monovalent ions.
@@ -17,61 +17,62 @@ function [ X, P ] = pb_3p_double_layer
 %       converged, repeat all.
 %   5. Adjust P closer to P_calc by ratio A.
 
-%   Michelle Shu | June 14, 2013
+%   Michelle Shu | Last modified on June 17, 2013
 
 % Constants
 N_A = 6.0221413e23;     % Avogadro's number (1/mol)
 E = 1.60217657e-19;     % Elementary charge (C)
 Z = 1;                  % Ion charge number
-E_R = 80;               % Relative permittivity (80 for H2O) 
+E_0 = 8.854187817e-12;  % Vacuum permittivity (F/m)   
 K = 1.3806488e-23;      % Boltzmann constant (J/K)
-T = 20;                 % Temperature (K)
+T = 293;                % Temperature (K)
 
 % Adjustable Parameters:
-P_0 = 0.1;              % Surface potential [Range: 25 - 200 mV] (V)
-C_0 = 1e-2;             % Bulk concentration (mol/m^3)
-H = 1e-8;               % Distance step size, determines resolution (m)
-A = 1e-11;              % Potential function adjustment step ratio
-                        %   (controls speed of convergence)
-C = 1;                  % Convergence criterion: Max acceptable ratio of
-                        %   (P_calc - P)/ P
-L = 1e-5;               % Limit for P to approach 0
+% Set these by passing in as parameters
+% P_0 = 0.025;            % Surface potential [Range: 25 - 200 mV] (V)
+% C_0 = 100;              % Bulk concentration (mol/m^3) -> M * 1e3
+% E_R = 80;               % Relative permittivity (80 for H2O)
 
-% Step 1. (Initialization)
+H = 1e-10;              % Distance step size, determines resolution (m)
+A = 0.1;                % Potential function adjustment step ratio
+                        %   (controls speed of convergence)
+C = 1e-6;               % Convergence criterion: Max acceptable ratio of
+                        %   (P_calc - P)/ P for an individual point
+L = 1e-8;               % Limit for P to approach 0
+G = 1e6;                % Steepness of initialization curve
+
+% Step 1. Initialization
 X = (0 : H : L);
-P = (- P_0 / L) .* X + P_0;
+%P = (- P_0 / L) .* X + P_0;     % linear guess
+P = (P_0 / (1 - exp(-G * L))) * (exp(-G * X) - exp(-G * L));   % exp. guess
 R = zeros(size(P));
 P_calc = zeros(size(P));
 P_calc(1) = P_0;        % fix at P_0
 done = false;
 
-while ~done
-    % Step 2.
+while ~done 
+    % Step 2. Poisson-Boltzmann equation
     for i = 1 : numel(P)
-        R(i) = (2 * Z * E * N_A * C_0 * sinh((Z * E * P(i)) / (K * T)) ...
-            / E_R);     % Poisson-Boltzmann equation
+        R(i) = (2 * Z * E * N_A * C_0 * sinh(Z * E * P(i) / (K * T))) / ...
+            (E_R * E_0);
     end
     
-    % Step 3.
-    residual_sum = 0;   % Compute these to prepare for step 4
-    P_sum = 0;
+    % Step 3. Three point formula
     for i = 2 : numel(P) - 1
-        P_calc(i) = (P(i - 1) + P(i + 1) - R(i) * H^2) / 2; % 3 point form.
-        residual_sum = residual_sum + abs(P_calc(i) - P(i));
-        P_sum = P_sum + P(i);
+        P_calc(i) = (P(i - 1) + P(i + 1) - R(i) * H^2) / 2;
     end
+    max_difference = max(abs(P_calc - P) ./ P);
+    disp(max_difference);
 
-    % Step 4.
-    disp(residual_sum / P_sum);
-    if (residual_sum / P_sum) < C
+    % Step 4. Test for convergence
+    if (max_difference) < C
         done = true;
     end
     
     % Step 5.
     P = P + (A * (P_calc - P));
-end
 
-plot(X, P);
+end
 
 end
 
